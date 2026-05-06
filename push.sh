d4ad23a66a83f10ea8764bba284ff1e5aa59e7a8
Stephen Piccolo <stephen.piccolo.byu@gmail.com>
2026-05-06 11:34:31 -0600

    Updated build number

---
+++ push.sh
@@ -0,0 +1,7 @@
+#: https://contributions.bioconductor.org/git-version-control.html#new-package-workflow
+
+## push to Bioc (upstream remote at git@git.bioconductor.org)
+git push upstream main:devel
+
+## push to GitHub (origin remote)
+git push origin main
